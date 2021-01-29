ff9e45daacbcc450726bca6e1c4095b497102d18
jacob <jacobcunningham96@yahoo.com>
2021-01-28 20:19:52 -0700

    made new tests and new aspects for e/ func

--- hello.go
+++ hello.go
@@ -4,22 +4,50 @@ import "fmt"
 
 const englishHelloPrefix = "Hello, "
 const englishGoodbyePrefix = "Goodbye, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const spanishGoodbyePrefix = "Adios, "
+const frenchGoodbyePrefix = "Au revoir, "
 
-func Hello(name string) string {
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case "French":
+		prefix = frenchHelloPrefix
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
 }
 
-func Goodbye(name string) string {
+func Goodbye(name string, language string) string {
 	if name == "" {
 		name = "Cruel World"
 	}
-	return englishGoodbyePrefix + name
+
+	prefix := englishGoodbyePrefix
+
+	switch language {
+	case "French":
+		prefix = frenchGoodbyePrefix
+	case "Spanish":
+		prefix = spanishGoodbyePrefix
+	}
+
+	return prefix + name
+
 }
 
 func main() {
-	fmt.Println(Hello("Chris"))
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", ""))
+	fmt.Println(Hello("Chris", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -12,14 +12,26 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("empty string defaults to 'World'", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
 
@@ -35,14 +47,20 @@ func TestGoodbye(t *testing.T) {
 	}
 
 	t.Run("Saying goodbye to people", func(t *testing.T) {
-		got := Goodbye("Chris")
+		got := Goodbye("Chris", "")
 		want := "Goodbye, Chris"
 		errorMessage(t, got, want)
 	})
 
 	t.Run("Saying goodbye to life", func(t *testing.T) {
-		got := Goodbye("")
+		got := Goodbye("", "")
 		want := "Goodbye, Cruel World"
+		errorMessage(t, got, want)
+	})
+
+	t.Run("goodbye in Spanish", func(t *testing.T) {
+		got := Goodbye("Elodie", "Spanish")
+		want := "Adios, Elodie"
 		errorMessage(t, got, want)
 	})
 }
